4d0fc5466525c4296a19a428bb553a696b0913cd
Gregory Leung <not.gregs.other.email@gmail.com>
2019-11-20 16:25:35 -0600

    Register route takes in request data

--- resources/users.py
+++ resources/users.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 import models
 
 users = Blueprint('users', __name__)
@@ -7,3 +7,31 @@ users = Blueprint('users', __name__)
 def test_user_controller():
     return "You are not logged in"
 
+@users.route('/register', methods=["POST"])
+def register_user():
+    #see if email taken and pword valid
+    payload = request.get_json()
+    user_email = payload['email'].lower()
+    payload['email'] = user_email
+    try:
+        models.User.get(models.User.email == user_email)
+        return jsonify(data={}, status={
+            'code': 401,
+            'message': 'The email {} is taken or reserved.'.format(user_email)
+        }), 401
+    except models.DoesNotExist:
+        #models.User.create(**payload)
+        return jsonify(data={}, status={
+            'code': 201,
+            'message': 'Successfully registered {}'.format(user_email)
+        }), 201
+
+@users.route('/login', methods=["POST"])
+def login_user():
+    #see if email exists and pword valid
+    return jsonify({'status': 'login'})
+
+@users.route('/logout', methods=["POST"])
+def logout_user():
+    #clear session
+    return jsonify({'status': 'logout'})
