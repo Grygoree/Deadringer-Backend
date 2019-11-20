9cf9e171caab67a7c16df15dbed71c71225fe8dd
Gregory Leung <not.gregs.other.email@gmail.com>
2019-11-20 17:09:58 -0600

    Can register a user

--- resources/users.py
+++ resources/users.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, request
+from flask_bcrypt import generate_password_hash
 import models
+from playhouse.shortcuts import model_to_dict
 
 users = Blueprint('users', __name__)
 
@@ -20,8 +22,11 @@ def register_user():
             'message': 'The email {} is taken or reserved.'.format(user_email)
         }), 401
     except models.DoesNotExist:
-        #models.User.create(**payload)
-        return jsonify(data={}, status={
+        hashed_pass = generate_password_hash(payload['password'])
+        payload['password'] = hashed_pass
+        created_user = models.User.create(**payload)
+        user_dict = model_to_dict(created_user)
+        return jsonify(data=user_dict, status={
             'code': 201,
             'message': 'Successfully registered {}'.format(user_email)
         }), 201
