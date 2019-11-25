d91b8c0b146c1d34c706686b310d147d0df3590f
Gregory Leung <not.gregs.other.email@gmail.com>
2019-11-25 16:43:44 -0600

    Can get received messages

--- resources/received.py
+++ resources/received.py
@@ -1,11 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 import models
+from flask_login import login_required, current_user
+from playhouse.shortcuts import model_to_dict
 
 received = Blueprint('received', __name__)
 
-@received.route('/', methods=["GET"])
+@received.route('', methods=["GET"])
+@login_required
 def get_inbox():
     """ Only returns messages whose trigger date is now """
+    try:
+        found_user = models.User.get_by_id(current_user.id)
+        received_messages_dicts = [model_to_dict(m) for m in found_user.received_messages]
+        for m in received_messages_dicts:
+            #Scrub passwords
+            del m['message']['author']['password']
+            del m['to_user']['password']
+        return jsonify(data=received_messages_dicts), 200
+    except models.DoesNotExist:
+        return "something went wrong"
     return "hitting inbox route"
 
 @received.route('/<id>', methods=["PUT"])
